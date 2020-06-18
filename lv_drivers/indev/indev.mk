9fd8d18d97249fdc401c5dbfe0799dc0e86f1ce3
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-06-18 19:15:43 -0400

    Remove unneeded input drivers

--- lv_drivers/indev/indev.mk
+++ lv_drivers/indev/indev.mk
@@ -1,10 +1,6 @@
-CSRCS += FT5406EE8.c
 CSRCS += keyboard.c
 CSRCS += mouse.c
 CSRCS += mousewheel.c
-CSRCS += evdev.c
-CSRCS += libinput.c
-CSRCS += XPT2046.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/lv_drivers/indev
 VPATH += :$(LVGL_DIR)/lv_drivers/indev
