05b3df42eb20bbe7c3eb7c77973eb70df56f77c2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-08 19:34:01 -0400

    lv_conf: Don't use LED

--- lv_conf.h
+++ lv_conf.h
@@ -499,7 +499,7 @@ typedef void * lv_obj_user_data_t;
 #endif
 
 /*LED (dependencies: -)*/
-#define LV_USE_LED      1
+#define LV_USE_LED      0
 
 /*Line (dependencies: -*/
 #define LV_USE_LINE     1
