cdf38a91f4ec95575b17cbf35ced05998e73b9a2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-11 23:32:33 -0400

    lv_nanosvg: Use URL-style parameters for tweaks

--- lv_lib_nanosvg/lv_nanosvg.c
+++ lv_lib_nanosvg/lv_nanosvg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "lvgl/lvgl.h"
 #include "lv_nanosvg.h"
+#include "url.h"
 
 #define NANOSVG_IMPLEMENTATION
 #include "nanosvg.h"
@@ -28,32 +29,46 @@ static lv_res_t decoder_info(struct _lv_img_decoder * decoder, const void * src,
 
 	(void) height;
 	(void) width;
-	(void) src_type;
 
 	if (src_type == LV_IMG_SRC_FILE) {
-		const char * filename = src;
+		const char * path = url_get_path(src);
 		NSVGimage *image = NULL;
 
-		image = nsvgParseFromFile(filename, "px", 96.0f);
+		image = nsvgParseFromFile(path, "px", 96.0f);
 		if (image == NULL) {
-			fprintf(stderr, "Could not open SVG image '%s' for information.\n", filename);
+			fprintf(stderr, "Could not open SVG image '%s' for information.\n", path);
 			nsvgDelete(image);
 
+			free(path);
 			return LV_RES_INV;
 		}
 
 		header->always_zero = 0;
 		header->cf = LV_IMG_CF_RAW_ALPHA;
 
+		// The internal width/height of the SVG
 		width = (int)image->width;
 		height = (int)image->height;
 
+		// The user might have requested specific dimensions via URL params
+		bool gave_width = url_get_int_param((char*)src, "width", &width);
+		bool gave_height = url_get_int_param((char*)src, "height", &height);
+
+		// User gave only one part of the component; scale accordingly
+		if ((gave_width || gave_height) && !(gave_width && gave_height)) {
+			if (gave_height) {
+				width = width * height / (int)image->height;
+			}
+			if (gave_width) {
+				height = height * width / (int)image->width;
+			}
+		}
 
 		header->w = width;
 		header->h = height;
 
 		nsvgDelete(image);
-
+		free(path);
 		return LV_RES_OK;
 	}
 
@@ -68,7 +83,7 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
 
 	(void) decoder;
 
-	const char * filename = dsc->src;
+	const char * filename = url_get_path(dsc->src);
 	NSVGimage *image = NULL;
 	NSVGrasterizer *rast = NULL;
 
@@ -77,6 +92,7 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
 		fprintf(stderr, "Could not open SVG image '%s' for rasterizing.\n", filename);
 		nsvgDelete(image);
 
+		free(filename);
 		return LV_RES_INV;
 	}
 
@@ -86,14 +102,22 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
 		nsvgDeleteRasterizer(rast);
 		nsvgDelete(image);
 
+		free(filename);
 		return LV_RES_INV;
 	}
 
-	dsc->img_data = malloc(dsc->header.w * dsc->header.h * 4);
+	dsc->img_data = lv_mem_alloc(dsc->header.w * dsc->header.h * 4);
+	LV_ASSERT_MEM(dsc->img_data);
+	if (dsc->img_data == NULL) {
+		free(filename);
+		return LV_RES_INV;
+	}
 	nsvgRasterize(rast, image, 0,0, ((float)dsc->header.w/(float)image->width), dsc->img_data, dsc->header.w, dsc->header.h, dsc->header.w*4);
 
+	nsvgDeleteRasterizer(rast);
 	nsvgDelete(image);
 
+	free(filename);
 	return LV_RES_OK;
 }
 
