9a5ce22700349592ac0968b3344e62bcbea0c9c6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-02-25 21:00:31 -0500

    default.nix: Build hello

--- default.nix
+++ default.nix
@@ -3,4 +3,11 @@
 rec {
   lvgui = pkgs.callPackage ./support/lvgui.nix {};
   lvgui-simulator = lvgui.override { withSimulator = true; };
+  hello = pkgs.callPackage ./support/hello {
+    inherit lvgui;
+  };
+  hello-simulator = pkgs.callPackage ./support/hello {
+    inherit lvgui;
+    withSimulator = true;
+  };
 }
