dbc8e5b72ade7d30f147de95f05ef5b85f1553d9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-08-03 19:37:41 -0400

    lv_drivers/libinput: Fix unused parameter

--- lv_drivers/indev/libinput.c
+++ lv_drivers/indev/libinput.c
@@ -522,12 +522,14 @@ bool libinput_read(lv_indev_drv_t * drv, lv_indev_data_t * data)
 
 static int open_restricted(const char *path, int flags, void *user_data)
 {
+	(void)user_data;
 	int fd = open(path, flags);
 	return fd < 0 ? -errno : fd;
 }
 
 static void close_restricted(int fd, void *user_data)
 {
+	(void)user_data;
 	close(fd);
 }
 
