45691822a46b9e9eb795e10363d811ecae823f70
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-08-03 18:41:15 -0400

    lv_lib_nanosvg: Value from url_get_path is owned / not const
    We have to free it. We can't when const.

--- lv_lib_nanosvg/lv_nanosvg.c
+++ lv_lib_nanosvg/lv_nanosvg.c
@@ -31,7 +31,7 @@ static lv_res_t decoder_info(struct _lv_img_decoder * decoder, const void * src,
 	(void) width;
 
 	if (src_type == LV_IMG_SRC_FILE) {
-		const char * path = url_get_path(src);
+		char * path = url_get_path(src);
 		NSVGimage *image = NULL;
 
 		image = nsvgParseFromFile(path, "px", 96.0f);
@@ -87,7 +87,7 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
 
 	(void) decoder;
 
-	const char * filename = url_get_path(dsc->src);
+	char * filename = url_get_path(dsc->src);
 	NSVGimage *image = NULL;
 	NSVGrasterizer *rast = NULL;
 
